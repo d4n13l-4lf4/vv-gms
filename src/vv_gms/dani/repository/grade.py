15bb2eb9d0af46e308407b74957fccf4e03741c5
Daniel Aucatoma <d.aucatoma96@gmail.com>
2025-11-10 23:05:50 +0100

    Setup deployment for onedir and onefile

---
+++ src/vv_gms/dani/repository/grade.py
@@ -0,0 +1,9 @@
+from typing import Dict, Any
+
+
+class GradeRepository:
+    def __init__(self, data: Dict[str, Any]):
+        self.__data_ = data
+
+    def get_grades_by_course_id_assignment(self):
+        pass
